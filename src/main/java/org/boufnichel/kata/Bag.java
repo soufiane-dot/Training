adbdee27db1c13f11414846724ab5a96049b3fea
ESIhabriFa <faycal.ihabritane@es.nestle.com>
2021-05-19 15:22:48 +0100

    Fix First UT

--- src/main/java/org/boufnichel/kata/Bag.java
+++ src/main/java/org/boufnichel/kata/Bag.java
@@ -1,10 +1,31 @@
 package org.boufnichel.kata;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Bag {
+
+    private static final String ITEM_SEPARATOR = "#";
+    private static final String ATTRIBUTE_SEPARATOR = "::";
+
+    private List<Item> items;
+
     public Bag(String s) {
+        items = new ArrayList<>();
+        createItemsFrom(s);
     }
 
     public double calculate() {
-        return 0d;
+        return items.stream().collect(Collectors.summingInt(Item::getPrice));
+    }
+
+    private void createItemsFrom(String token) {
+        String[] stringItems = token.split(ITEM_SEPARATOR);
+        Arrays.stream(stringItems).forEach(item -> {
+            String[] itemAttribute = item.split(ATTRIBUTE_SEPARATOR);
+            items.add(new Item(itemAttribute[0], Integer.valueOf(itemAttribute[1])));
+        });
     }
 }
---
+++ src/main/java/org/boufnichel/kata/Item.java
@@ -0,0 +1,20 @@
+package org.boufnichel.kata;
+
+public class Item {
+
+    private String name;
+    private Integer price;
+
+    public Item(String name, Integer price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+}
