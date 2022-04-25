f6049c4c2d07fbc0cf611e6d199bd766faaf919f
mohamed.boufnichel <mohamed.boufnichel>
2022-04-25 15:03:10 +0000

    new cases

--- src/test/java/org/boufnichel/kata/CheckoutTest.java
+++ src/test/java/org/boufnichel/kata/CheckoutTest.java
@@ -11,31 +11,30 @@ class CheckoutTest {
     @Test
     @DisplayName("Init bag")
     void test_simple_item() {
-        Bag bag = new Bag("Cornflex::50#Milka::10");
+        Bag bag = new Bag("Nutella::88#Milka::20");
         assertEquals(2, bag.size());
-        assertEquals(60, bag.calculate());
-
+        assertEquals(108, bag.calculate());
     }
 
     @Test
     @DisplayName("Quantity")
     void test_simple_item_quantity() {
-        Bag bag = new Bag("Cornflex:2:50#Milka:1:10");
+        Bag bag = new Bag("Nutella:2:88#Milka:1:20");
         assertEquals(2, bag.size());
-        assertEquals(110, bag.calculate());
+        assertEquals(196, bag.calculate());
     }
 
     @Test
     @DisplayName("Remove entry")
     void test_remove_entry() {
-        Bag bag = new Bag("Cornflex:2:50#Milka:1:10");
+        Bag bag = new Bag("Nutella:2:88#Milka:1:20");
         assertEquals(2, bag.size());
-        assertEquals(110, bag.calculate());
+        assertEquals(196, bag.calculate());
 
-        bag.remove("Cornflex");
+        bag.remove("Nutella");
 
         assertEquals(1, bag.size());
-        assertEquals(10, bag.calculate());
+        assertEquals(20, bag.calculate());
 
     }
 
